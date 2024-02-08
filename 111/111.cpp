eec2a4c26080a9ebda57e3f78acb69a77b4129bc
User <User@HOME-PC>
2024-02-08 22:31:48 +0300

    Добавьте файлы проекта.

---
+++ 111/111.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+template<typename T>
+class Swapper {
+public:
+    void Swap(T& x, T& y) {
+        T q = x;
+        x = y;
+        y = q;
+    }
+    void Print(T& x, T& y) {
+        cout << "Swap: " << x << endl;
+        cout << "Swap: " << y << endl;
+    }
+};
+
+int main()
+{
+    int x = 10;
+    int y = 5;
+
+    Swapper<int> swapping;
+    swapping.Print(x, y);
+    swapping.Swap(x, y);
+    swapping.Print(x, y);
+}
